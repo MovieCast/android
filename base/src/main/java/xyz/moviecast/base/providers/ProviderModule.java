ec3cf57e3f87e3094e5eb1f7876e998852e6f4cf
ItsDizzy <itsdizzy40@gmail.com>
2018-05-16 12:14:08 +0200

    Added Copyright notice to all files

--- base/src/main/java/xyz/moviecast/base/providers/ProviderModule.java
+++ base/src/main/java/xyz/moviecast/base/providers/ProviderModule.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) MovieCast and it's contributors. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for license information.
+ */
+
 package xyz.moviecast.base.providers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
--- mobile/src/main/java/xyz/moviecast/fragments/ShowDetailFragment.java
+++ mobile/src/main/java/xyz/moviecast/fragments/ShowDetailFragment.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) MovieCast and it's contributors. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for license information.
+ */
+
 package xyz.moviecast.fragments;
 
 import android.annotation.TargetApi;
