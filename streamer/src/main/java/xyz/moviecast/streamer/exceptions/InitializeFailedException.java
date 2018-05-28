8b820b3579e21baf191fff05ccc05794fcf39888
Dizzy <itsdizzy40@gmail.com>
2018-05-28 11:11:36 +0200

    Forgot to push these two files

--- streamer/src/main/java/xyz/moviecast/streamer/exceptions/InitializeFailedException.java
+++ streamer/src/main/java/xyz/moviecast/streamer/exceptions/InitializeFailedException.java
@@ -1,4 +1,15 @@
 package xyz.moviecast.streamer.exceptions;
 
 public class InitializeFailedException extends RuntimeException {
+
+    private String reason;
+
+    public InitializeFailedException(String reason) {
+        super("MovieCast Streamer failed to initialize, reason: " + reason);
+        this.reason = reason;
+    }
+
+    public String getReason() {
+        return reason;
+    }
 }
