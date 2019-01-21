56adee177ed428070b1e540efff216d05d19768b
Tim <timtim3001@gmail.com>
2019-01-21 02:04:13 +0100

    Added fullscreen to player activity

--- mobile/src/main/java/io/moviecast/activities/PlayerActivity.java
+++ mobile/src/main/java/io/moviecast/activities/PlayerActivity.java
@@ -5,9 +5,11 @@
 
 package io.moviecast.activities;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.FrameLayout;
 
@@ -23,6 +25,8 @@ public class PlayerActivity extends AppCompatActivity {
     @BindView(R.id.video_player)
     FrameLayout videoPlayer;
 
+    private final Handler hideHandler = new Handler();
+
     public static Intent getIntent(Context context) {
         return new Intent(context, PlayerActivity.class);
     }
@@ -36,5 +40,17 @@ public class PlayerActivity extends AppCompatActivity {
         VideoPlayerFragment videoPlayerFragment = new VideoPlayerFragment();
         getSupportFragmentManager().beginTransaction().replace(R.id.video_player, videoPlayerFragment).commit();
         videoPlayerFragment.setVideoPath("https://vjs.zencdn.net/v/oceans.mp4");
+    }
+
+    @Override
+    @SuppressLint("InlinedApi")
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        hideHandler.postDelayed(() -> videoPlayer.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION), 100);
     }
 }
